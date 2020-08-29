b6a68f40025697356f8522baf62f1b802fe26a7c
adushaj2022 <47046091+adushaj2022@users.noreply.github.com>
2020-08-29 19:57:31 -0400

    Update Cpu.ts
    Child Class CPU

--- src/hardware/Cpu.ts
+++ src/hardware/Cpu.ts
@@ -1,9 +1,33 @@
 import {System} from "../System";
+import { hardware } from "./hardware";
 
-export class Cpu {
+export class Cpu extends hardware {
 
-    constructor() {
+    constructor(idNumber: number, name: String) {
         
+       super(idNumber, name);
         
+       this.startSystem();
     }
+
+
+
+
+    public log(message: String){
+
+        return super.log(message);
+    }
+
+    public startSystem(): boolean {
+
+        return super.startSystem();
+    }
+
+    public stopSystem(): boolean {
+
+        return super.stopSystem();
+
+    }
+
+
 }
